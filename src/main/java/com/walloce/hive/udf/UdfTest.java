00d08fe6785e919ffef7c2042beedb2b23861baa
Walloce <Walloce@DESKTOP-27SCR1L>
2018-04-11 23:21:35 +0800

    添加测试类

--- src/main/java/com/walloce/hive/udf/UdfTest.java
+++ src/main/java/com/walloce/hive/udf/UdfTest.java
@@ -2,9 +2,18 @@ package com.walloce.hive.udf;
 
 import org.apache.hadoop.io.Text;
 
+/**
+ * 
+ * @Title: UdfTest.java  
+ * @Package com.walloce.hive.udf  
+ * @Description: 测试类
+ * @author Walloce  
+ * @date 2018年4月11日
+ */
 public class UdfTest {
 
 	public static void main(String[] args) {
+		
 		String date = "31/Aug/2015:00:04:37 +0800";
 		String dateFormated = new DateUDF().evaluate(date);
 		System.out.println(dateFormated.toString());
